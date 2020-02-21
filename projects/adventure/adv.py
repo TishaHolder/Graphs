c4852d1a8940042d5a8f6390a280a568512d39e8
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-02-20 23:40:36 -0500

    lots of moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -6,93 +6,108 @@ from util import Stack, Queue
 import random
 from ast import literal_eval
 
-#unexplored rooms have a ?
-#visited rooms have a direction filled in
-
 # Load world
 world = World()
-
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/test_loop_fork.txt"
 
-#map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
-# Print an ASCII map
-world.print_rooms()
+#print the room graph dictionary for testing purposes
+#print(room_graph)
 
-#this statment in world.py (self.starting_room = self.rooms[0]) sets starting_room to rooms[0]
+# Print an ASCII map
+#world.print_rooms()
+
+#this statment in world.py (self.starting_room = self.rooms[0]) sets starting_room to the first room rooms[0]
+#puts the player in the first room
 player = Player(world.starting_room)
+
+#You may find the following commands useful:
+#player.current_room.id - returns current room id, 
+#player.current_room.get_exits - every exit in the current_room.get_exits() that leads somewhere 
+#player.travel(direction) - if there is a room in the selected direction change current room to that room 
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-class Graph:
+#empty set to store visited rooms
+visited = set()
 
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
-    #a way of storing the connections
-    #each key is the name of the vertex(node) and the value for each key is an array with the associated edges
-    def __init__(self):
-        #stores the relationships between the vertices and the edges
-        #creates a hash table called rooms to store the nodes and their connected edges
-        self.rooms = {}
+#dictionary with opposite directions
+opposite_directions = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}        
 
-    #a vertex is a node or one of the circles
-    #before we can draw the connections we need to add the nodes
-    def add_room(self, room_id):
-        """
-        Add a vertex to the graph.
-        """       
-        #sets have no duplicates, have to be unique
-        # unordered so order doesn't matter
-        # 0(1) or very fast
-        # sets are just hash tables underneath the hood without keys 
-        if room_id in self.rooms:
-            print("WARNING: That room already exists")
-        else:
-            #adding a directed edge
-            self.rooms[room_id] = set()
+#create an empty stack
+stack = Stack()
 
-    #add pathway is entering a direction
-    def add_pathway(self, room1, room2):
-        """
-        Add a directed edge to the graph.
-        """
-        #if v1 and v2 are vertexes or nodes
-        #add a directed or one way edge
-        #.add is how we add something to a set
-        if room1 == room2:
-            print("WARNING: You cannot add a pathway that leads from your current room back to your current room")
-        elif room1 in self.rooms and room2 in self.vertices:
-            self.rooms[room1].add(room2)
-            self.rooms[room2].add(room1)            
-        else:
-            raise IndexError("That room does not exist")        
+#place player in the first room
+player.current_room = world.starting_room
 
+#add the first room to the visited set
+visited.add(player.current_room.id)
 
-    def get_neighbors(self, room_id):
-        """
-        Get all neighbors (edges) of a vertex.
-        """        
-        #find vertex_id in the dictionary and return all it's values
-        return self.rooms[room_id]
+#move from room to room while there are unexplored rooms
+#You start in room 0, which contains exits ['n', 's', 'w', 'e']
+while len(visited) != len(room_graph):
+
+    #the exits for a room will contain either [n, s, e, or w] but not necessarily all of them
+    #only the exits that lead to somewhere 
+    #get all the exits for the current room
+    exits = player.current_room.get_exits()  
+
+    #choose a random direction to move from the exits list               
+    direction_to_move = random.choice(exits)  
+   
+    #if the current room is not in visited
+    if player.current_room.id not in visited:
+        #add it to the visited set
+        visited.add(player.current_room.id)       
+
+    #if there is a n in the list of valid exits for a room
+    #check if there is a room in that direction and if was already visited
+    if direction_to_move in exits and player.current_room.get_room_in_direction(direction_to_move) not in visited:
+
+            #add it to the stack to keep track of directions
+            stack.push(direction_to_move)
+
+            #if it wasn't already visited add the direction to traversal_path
+            traversal_path.append(direction_to_move)
+
+            #go in that direction
+            #if there is a room in the selected direction 
+            #player.current_room.get_room_in_direction(direction_to_move) will 
+            #change current room to that room           
+            player.current_room = player.current_room.get_room_in_direction(direction_to_move)  
+       
+            #now the loop will iterate with the new player.current_room.id
+    #if the direction was already visited
+    else:
+            #remove it from the stack          
+            direction_to_move = stack.pop()
+
+            #get the reverse direction from the oppoite_directions dictionary
+            reverse_direction = opposite_directions.get(direction_to_move)
+
+            #add the reverse direction to traversal_path
+            traversal_path.append(reverse_direction)
+
+            #change current room to that room           
+            player.current_room = player.current_room.get_room_in_direction(reverse_direction)         
 
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
-
-#write a traversal method
-
 
 #for every direction in traversal_path = [n, s, e, w]
 for move in traversal_path:
@@ -115,7 +130,7 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
+"""player.current_room.print_room_description(player)
 while True:
     cmds = input("-> ").lower().split(" ")
     if cmds[0] in ["n", "s", "e", "w"]:
@@ -123,4 +138,4 @@ while True:
     elif cmds[0] == "q":
         break
     else:
-        print("I did not understand that command.")
+        print("I did not understand that command.")"""
