01845b2182caaf5a6f01ddee6ac957c6b4aea11a
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-02-21 15:41:05 -0500

    less than 2000 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -31,6 +31,9 @@ world.load_graph(room_graph)
 #puts the player in the first room
 player = Player(world.starting_room)
 
+#make the first room the current room
+current_room = world.starting_room
+
 #You may find the following commands useful:
 #player.current_room.id - returns current room id, 
 #player.current_room.get_exits - every exit in the current_room.get_exits() that leads somewhere 
@@ -42,6 +45,9 @@ traversal_path = []
 
 #empty set to store visited rooms
 visited = set()
+
+#dictionary with regular directions
+directions = {"north": "n", "south": "s", "east": "e", "west": "w"} 
 
 #dictionary with opposite directions
 opposite_directions = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}        
@@ -55,53 +61,109 @@ player.current_room = world.starting_room
 #add the first room to the visited set
 visited.add(player.current_room.id)
 
+visited_rooms = set()
+
 #move from room to room while there are unexplored rooms
 #You start in room 0, which contains exits ['n', 's', 'w', 'e']
 while len(visited) != len(room_graph):
+    visited_rooms.add(current_room)
 
+    
     #the exits for a room will contain either [n, s, e, or w] but not necessarily all of them
     #only the exits that lead to somewhere 
     #get all the exits for the current room    
-    exits = player.current_room.get_exits()   
+    exits = current_room.get_exits()   
   
     #choose a random direction to move from the exits list               
-    direction_to_move = random.choice(exits)           
+    #direction_to_move = random.choice(exits)   
+        
    
     #if the current room is not in visited
-    if player.current_room.id not in visited:
+    if current_room.id not in visited:
         #add it to the visited set
-        visited.add(player.current_room.id)       
+        visited.add(current_room.id) 
 
-    #if there is a n in the list of valid exits for a room
+    #reverse_direction = opposite_directions.get(direction_to_move)    
+           
+
+    #if there is a w in the list of valid exits for a room
     #check if there is a room in that direction and if was already visited
-    if direction_to_move in exits and player.current_room.get_room_in_direction(direction_to_move) not in visited:
+    if "w" in exits and current_room.get_room_in_direction("w") not in visited_rooms:
 
             #add it to the stack to keep track of directions
-            stack.push(direction_to_move)
+            stack.push("w")
 
             #if it wasn't already visited add the direction to traversal_path
-            traversal_path.append(direction_to_move)
+            traversal_path.append("w")
 
             #go in that direction
             #if there is a room in the selected direction 
             #player.current_room.get_room_in_direction(direction_to_move) will 
             #change current room to that room           
-            player.current_room = player.current_room.get_room_in_direction(direction_to_move)  
+            current_room = current_room.get_room_in_direction("w")  
        
             #now the loop will iterate with the new player.current_room.id
+    elif "s" in exits and current_room.get_room_in_direction("s") not in visited_rooms:
+
+            #add it to the stack to keep track of directions
+            stack.push("s")
+
+            #if it wasn't already visited add the direction to traversal_path
+            traversal_path.append("s")
+
+            #go in that direction
+            #if there is a room in the selected direction 
+            #player.current_room.get_room_in_direction(direction_to_move) will 
+            #change current room to that room           
+            current_room = current_room.get_room_in_direction("s")  
+       
+            #now the loop will iterate with the new player.current_room.id
+    elif "n" in exits and current_room.get_room_in_direction("n") not in visited_rooms:
+
+            #add it to the stack to keep track of directions
+            stack.push("n")
+
+            #if it wasn't already visited add the direction to traversal_path
+            traversal_path.append("n")
+
+            #go in that direction
+            #if there is a room in the selected direction 
+            #player.current_room.get_room_in_direction(direction_to_move) will 
+            #change current room to that room           
+            current_room = current_room.get_room_in_direction("n")  
+       
+            #now the loop will iterate with the new player.current_room.id
+
     #if the direction was already visited
+    elif "e" in exits and current_room.get_room_in_direction("e") not in visited_rooms:
+
+            #add it to the stack to keep track of directions
+            stack.push("e")
+
+            #if it wasn't already visited add the direction to traversal_path
+            traversal_path.append("e")
+
+            #go in that direction
+            #if there is a room in the selected direction 
+            #player.current_room.get_room_in_direction(direction_to_move) will 
+            #change current room to that room           
+            current_room = current_room.get_room_in_direction("e")  
+       
+            #now the loop will iterate with the new player.current_room.id    
+
+    #if a "?" was found
     else:
-            #remove it from the stack          
+            #remove the last valid direction from the stack        
             direction_to_move = stack.pop()
 
-            #get the reverse direction from the oppoite_directions dictionary
+            #and get the reverse direction from the oppoite_directions dictionary
             reverse_direction = opposite_directions.get(direction_to_move)
 
             #add the reverse direction to traversal_path
             traversal_path.append(reverse_direction)
 
-            #change current room to that room           
-            player.current_room = player.current_room.get_room_in_direction(reverse_direction) 
+            #change current room to back to the previous or opposite room          
+            current_room = current_room.get_room_in_direction(reverse_direction) 
         
     
 
